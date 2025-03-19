2b956e6104ec498508759e673e9dc5fa8cd4ad4b
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-03-19 14:56:49 +0700

    [add] login

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
+import { UseFormSetError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
+import { EntityError } from "./http";
+import { toast } from "@/components/ui/use-toast";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -25,4 +28,32 @@ export const removeTokensFromLocalStorage = () => {
   isBrowser && localStorage.removeItem("accessToken");
   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
   isBrowser && localStorage.removeItem("refreshToken");
+};
+
+export const handleErrorApi = ({
+  error,
+  setError,
+  duration,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setError?: UseFormSetError<any>;
+  duration?: number;
+}) => {
+  if (error instanceof EntityError && setError) {
+    error.payload.errors.forEach((item) => {
+      setError(item.field, {
+        type: "server",
+        message: item.message,
+      });
+    });
+  } else {
+    toast({
+      title: "Lỗi",
+      description: error?.payload?.message ?? "Lỗi không xác định",
+      variant: "destructive",
+      duration: duration ?? 5000,
+    });
+  }
 };
