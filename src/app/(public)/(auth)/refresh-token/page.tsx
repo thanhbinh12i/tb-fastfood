273ae2b3ee86a3a864a6400695030c21e9cbc9e4
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-03-29 13:45:39 +0700

    no cmt

---
+++ src/app/(public)/(auth)/refresh-token/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import {
+  checkAndRefreshToken,
+  getRefreshTokenFromLocalStorage,
+} from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RefreshTokenPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const refreshTokenFromUrl = searchParams.get("refreshToken");
+  const redirectPathName = searchParams.get("redirect");
+  useEffect(() => {
+    if (
+      refreshTokenFromUrl &&
+      refreshTokenFromUrl === getRefreshTokenFromLocalStorage()
+    ) {
+      checkAndRefreshToken({
+        onSuccess: () => {
+          router.push(redirectPathName || "/");
+        },
+      });
+    }
+  }, [router, refreshTokenFromUrl, redirectPathName]);
+  return <div>RefreshTokenPage</div>;
+}
