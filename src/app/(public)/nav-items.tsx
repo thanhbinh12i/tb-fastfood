9133a3d95de505512e42ab977d9cb7b84324c483
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-04-16 13:49:24 +0700

    update nav-item

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import { useAppContext } from "@/components/app-provider";
+import { Role } from "@/constants/type";
+import { cn, handleErrorApi } from "@/lib/utils";
+import { useLogoutMutation } from "@/queries/useAuth";
+import { useGuestLogoutMutation } from "@/queries/useGuest";
+import { RoleType } from "@/types/jwt.types";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const menuItems = [
+const menuItems: {
+  title: string;
+  href: string;
+  role?: RoleType[];
+  hideWhenLogin?: boolean;
+}[] = [
+  {
+    title: "Trang chủ",
+    href: "/",
+  },
   {
     title: "Món ăn",
-    href: "/menu",
+    href: "/guest/menu",
+    role: [Role.Guest],
   },
   {
     title: "Đơn hàng",
@@ -15,27 +31,56 @@ const menuItems = [
   {
     title: "Đăng nhập",
     href: "/login",
-    authRequired: false,
+    hideWhenLogin: true,
   },
   {
     title: "Quản lý",
     href: "/manage/dashboard",
-    authRequired: true,
+    role: [Role.Employee, Role.Owner],
   },
 ];
 
 export default function NavItems({ className }: { className?: string }) {
-  const { isAuth } = useAppContext();
-  return menuItems.map((item) => {
-    if (
-      (item.authRequired === false && isAuth) ||
-      (item.authRequired === true && !isAuth)
-    )
-      return null;
-    return (
-      <Link href={item.href} key={item.href} className={className}>
-        {item.title}
-      </Link>
-    );
-  });
+  const { role, setRole } = useAppContext();
+  const router = useRouter();
+  const logoutMutation = useLogoutMutation();
+  const guestLogoutMutation = useGuestLogoutMutation();
+  const logout = async () => {
+    if (logoutMutation.isPending || guestLogoutMutation.isPending) return;
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      role === Role.Guest
+        ? await guestLogoutMutation.mutateAsync()
+        : await logoutMutation.mutateAsync();
+      setRole();
+      router.push("/");
+    } catch (error) {
+      handleErrorApi({ error });
+    }
+  };
+  return (
+    <>
+      {menuItems.map((item) => {
+        // Trường hợp đăng nhập thì chỉ hiển thị menu đăng nhập
+        const isAuth = item.role && role && item.role.includes(role);
+        // Trường hợp menu item có thể hiển thị dù cho đã đăng nhập hay chưa
+        const canShow =
+          (item.role === undefined && !item.hideWhenLogin) ||
+          (!role && item.hideWhenLogin);
+        if (isAuth || canShow) {
+          return (
+            <Link href={item.href} key={item.href} className={className}>
+              {item.title}
+            </Link>
+          );
+        }
+        return null;
+      })}
+      {role && (
+        <div className={cn(className, "cursor-pointer")} onClick={logout}>
+          Đăng xuất
+        </div>
+      )}
+    </>
+  );
 }
